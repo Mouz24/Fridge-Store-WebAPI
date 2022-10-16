c12b8f048f8d100cb44ec16f853bf57b03c25e68
Mouz24 <90099528+Mouz24@users.noreply.github.com>
2022-10-16 21:03:18 +0300

    Done

---
+++ Contracts/IFridgeProductsRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities.DataTransferObjects;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IFridgeProductsRepository
+    {
+        IEnumerable<fridge_products> GetFridgeProducts(bool trackChanges);
+        IEnumerable<fridge_products> GetFridge_Products_By_Id(Guid FridgeId, bool trackChanges);
+        IEnumerable<ProductsInFridgeDTO> JoinFridgeIdWithProducts(IEnumerable<products> products, IEnumerable<FridgeProductsDTO> fridgeproducts);
+        IEnumerable<ProductsInFridgeWithDescriptionDTO> JoinFridgeDescriptionModelsWithProductsInFridge(IEnumerable<FridgeWithModelsDTO> fridgeWithModels,
+                                                        IEnumerable<ProductsInFridgeDTO> productsInFridge);
+        void PutProductinFridge(Guid FridgeId, fridge_products products, products productsDTO, ProductsForAddingDTO productstoadd);
+        void RemoveProductFromFridge(fridge_products product);
+        fridge_products GetProductInFridge(Guid FridgeId, Guid Id, bool trackChanges);
+        fridge_products GetProductInAllFridges(Guid Id, bool trackChanges);
+        bool FridgeProductsDTOIsEmpty(FridgeProductsDTO fridgeProductsDTO);
+        bool FridgeDTOIsEmpty(FridgeDTO fridgeProductsDTO);
+
+    }
+}
---
+++ Contracts/IFridgeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Entities;
+using Entities.Models;
+using Entities.DataTransferObjects;
+
+namespace Contracts
+{
+    public interface IFridgeRepository
+    {
+        IEnumerable<fridge> GetAllFridges(bool trackChanges);
+        IEnumerable<fridge> GetFridgeById(Guid fridgeid, bool trackChanges);
+        IEnumerable<FridgeWithModelsDTO> JoinFridgeWithModel(IEnumerable<FridgeDTO> fridgeDTO, IEnumerable<fridge_model> fridgemodel);
+        fridge FindDublicateFridgeByName(FridgeForAddingDTO Fridge, bool trackChanges);
+        fridge OtherFridgesOfThisModel(Guid ModelId, bool trackChanges);
+        fridge GetFridge(Guid fridgeid, bool trackChanges);
+        void AddFridge(fridge fridge);
+        void DeleteFridge(fridge fridge);
+
+    }
+}
